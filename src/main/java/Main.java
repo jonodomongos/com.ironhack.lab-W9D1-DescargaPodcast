33a4ccf91a6bbfc1c0cfbb209fb7505af48b0fa5
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-10-20 15:08:06 +0200

    commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+    /*
+    INSTRUCCIONES:
+    Desde cero, genera tres tablas para el podcast: usuario, descargas, podcast.
+    Puedes generarlas desde el modelo visual o desde el interface habitual de WorkBench.
+    La tabla de descargas debe guardar su momento de descarga como dato.
+    ¿Cómo las relacionas?
+    Inserta registros en todas las tablas. ¿Qué problemas encuentras?
+    Luego, prueba a hacer consultas multi-tabla, del tipo: ¿qué podcast se ha descargado tal cliente?
+    */
+    }
+}
